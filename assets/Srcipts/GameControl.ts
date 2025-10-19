6d7d6fd0a471cbed5ae967834b0eaca5101ef358
Max <Maximviktorovic2305>
2025-10-19 20:34:46 +1000

    add init game

---
+++ assets/Srcipts/GameControl.ts
@@ -0,0 +1,22 @@
+import { _decorator, CCInteger, Component, Node } from 'cc'
+import { Ground } from './Ground'
+const { ccclass, property } = _decorator
+
+@ccclass('GameControl')
+export class GameControl extends Component {
+	@property({ type: Ground }) public ground: Ground
+	@property({ type: CCInteger }) public speed: number = 300
+	@property({ type: CCInteger }) public pipeSpeed: number = 200
+
+    onLoad(): void {
+        
+    }         
+
+    initListener() {
+
+    }
+
+    startGame() {
+        
+    }
+}
